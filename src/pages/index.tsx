7f12711652cb7a4d8c38bbead408517b3c4968b3
Rui Junior <77105367+rui-junior@users.noreply.github.com>
2025-03-11 15:17:19 -0300

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,3 @@
-// import localFont from "next/font/local";
-// import styles from "@/styles/Home.module.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 import { useState } from "react";
 import { destroyCookie, setCookie } from "nookies";
@@ -24,7 +11,7 @@ export default function Home() {
 
   // Função que o Pai passa para o Filho
   const handleDataFromChild = (data: string) => {
-    setMessage(data); // Atualiza o estado com os dados enviados pelo Filho
+    setMessage(data);
   };
 
   return (
@@ -39,7 +26,7 @@ export async function getServerSideProps(context: any) {
   const token = context.req.cookies?.token;
 
   try {
-    // Verifica se o token existe antes de tentar decodificar
+
     if (!token) {
       destroyCookie(context, "token", { path: "/" });
       return { props: {} };
@@ -56,14 +43,12 @@ export async function getServerSideProps(context: any) {
     };
 
   } catch (error: any) {
-    // Lógica para tratar erros do Firebase ou token inválido
-    // console.log("Erro ao verificar o token:", error);
 
     destroyCookie(context, "token", { path: "/" });
 
     return {
       redirect: {
-        destination: "/login", // Redireciona para a página de login
+        destination: "/login",
         permanent: false,
       },
     };
